6e1d0c65a8a640fb13987057c52b6294fb7d01a0
Syskey-Alex <alexionita0207@yahoo.com>
2022-03-15 10:16:32 +0000

    Initial commit

---
+++ src/com/company/Stack.java
@@ -0,0 +1,29 @@
+package com.company;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class Stack extends List {
+    Node top = null;
+    Node node = new Node();
+    public Stack() {
+        super();
+        top = null;
+    }
+
+    public void push(int n) {
+    addFirst
+
+        top.setNext(Node);
+    top.getNext();
+    }
+public int pop() {
+        int v = head.getValue();
+        head = head.getNextLink();
+        return v;
+}
+public int peek() {
+        //just returns the value of the without changing it, hence just "peeking" at the stack
+        return head.getValue();
+}
+}
